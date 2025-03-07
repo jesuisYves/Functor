4fde9514af211d1c6986703692d56f28876a642f
Ivan Huz <vanahuz@gmail.com>
2025-03-08 00:30:57 +0200

    Move redundant wrapping prevention to constructor

--- JavaScript/5-proto-ap.js
+++ JavaScript/5-proto-ap.js
@@ -1,12 +1,13 @@
 'use strict';
 
 function Maybe(x) {
+  if (x instanceof Maybe) return x;
   this.x = x;
 }
 
 Maybe.prototype.map = function(fn) {
   const res = (this.x && fn) ? fn(this.x) : null;
-  return res instanceof Maybe ? res : new Maybe(res);
+  return new Maybe(res);
 };
 
 Maybe.prototype.ap = function(functor) {
@@ -20,4 +21,3 @@ const f1 = new Maybe((x) => x * 2);
 const f2 = new Maybe((x) => ++x);
 
 a.ap(f1).ap(f2).map(console.log);
-
